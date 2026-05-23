776a453d696f2cf7e17fa7047321da1c97b0bd45
Nguyen Quoc Viet <nguyenqviet3885@gmail.com>
2026-05-23 07:53:36 +0700

    feat: add web route to trigger storage link creation

--- be-3d/routes/web.php
+++ be-3d/routes/web.php
@@ -9,3 +9,13 @@ Route::get('/', function () {
 Route::get('/api/docs', function () {
     return view('api_docs');
 });
+
+Route::get('/run-storage-link', function () {
+    try {
+        \Illuminate\Support\Facades\Artisan::call('storage:link');
+        return "Storage link created successfully!";
+    } catch (\Exception $e) {
+        return "Error creating storage link: " . $e->getMessage();
+    }
+});
+
